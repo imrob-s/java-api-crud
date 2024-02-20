1245373b30ca75878bb2fdd1dff8f974d4812c16
imrob-s <orobsilva@gmail.com>
2024-02-20 17:27:03 -0300

    add: gradle project files

---
+++ src/test/java/com/imrob/javaspringcrud/JavaSpringCrudApplicationTests.java
@@ -0,0 +1,13 @@
+package com.imrob.javaspringcrud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaSpringCrudApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
